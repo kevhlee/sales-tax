dbd8c46457d5804bd9babe54d240b53bc9fdda34
Kevin Lee <kevin.lee@liferay.com>
2022-03-08 10:38:51 -0800

    Pull up method (350): pull up try-block to remove duplication

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,13 +3,12 @@ import java.net.*;
 import java.util.*;
 import java.text.*;
 
-class HelloWorld
-{
+class HelloWorld {
+
 	public static final double SALES_TAX = 0.10;
 	public static final double IMPORT_TAX = 0.05;
 
-	public static void main (String[] args) 
-	{
+	public static void main(String[] args) {
 
 		String[] items = new String[5];
 		int[] amount = new int[5];
@@ -31,155 +30,117 @@ class HelloWorld
 
 		// Prompt user to enter how many items were purchased
 		System.out.print("Please enter how many items were purchased: ");
-			try
-			{
-				input = br.readLine();
-				itemsPurchased = Integer.parseInt(input);
-			} 
-			catch (IOException e) 
-			{
-				System.out.println("IO error trying to read how many items purchased.");
-				System.exit(1);
-			}
 
+		try {
+			input = br.readLine();
+			itemsPurchased = Integer.parseInt(input);
+		}
+		catch (IOException e) {
+			System.out.println("IO error trying to read how many items purchased.");
+			System.exit(1);
+		}
 
 		// For loop to read through all items purchased
 		// Take input from the user for item name, amount, and cost
 
-		for (int i = 0; i < itemsPurchased; i++)
-		{
-			System.out.print("\n" + "Item #" + (i + 1) + "\n");
-			// Input the item purchased
-			System.out.print("What item did you purchase: ");
-				try
-				{
-					items[i] = br.readLine();
-				} 
-				catch (IOException e) 
-				{
-					System.out.println("IO error trying to read what item was purchased.");
-					System.exit(1);
-				}
+		try {
+			for (int i = 0; i < itemsPurchased; i++) {
+				System.out.print("\n" + "Item #" + (i + 1) + "\n");
 
-			// Input how many were purchased
-			System.out.print("How many did you purchase: ");
-				try
-				{
+				// Input the item purchased
+				System.out.print("What item did you purchase: ");
+
+				items[i] = br.readLine();
+
+				// Input how many were purchased
+				System.out.print("How many did you purchase: ");
+
+				input = br.readLine();
+				amount[i] = Integer.parseInt(input);
+
+				// Input cost of item purchased
+				System.out.print("How much did it cost: ");
+
+				input = br.readLine();
+				cost[i] = Double.parseDouble(input);
+				cost[i] = (cost[i] * amount[i]);
+
+				// Ask the user if it is a food item
+				inputFlag = true;
+
+				while (inputFlag) {
+					System.out.print("Is this a food, book, or medical item (Y or N): ");
 					input = br.readLine();
-					amount[i] = Integer.parseInt(input);
-				} 
-				catch (IOException e) 
-				{
-					System.out.println("IO error trying to read what item was purchased.");
-					System.exit(1);
+
+					if (input.equals("Y") || input.equals("y") || input.equals("N") || input.equals("n")) {
+						exempt[i] = input;
+						inputFlag = false;
+					}
+					else {
+						System.out.print("Please enter 'Y' or 'N': ");
+					}
 				}
 
-			// Input cost of item purchased
-			System.out.print("How much did it cost: ");
-				try
-				{
+				// Ask the user if it is an imported item
+				inputFlag = true;
+				while (inputFlag) {
+					System.out.print("Is this an imported item (Y or N): ");
 					input = br.readLine();
-					cost[i] = Double.parseDouble(input);
-					cost[i] = (cost[i] * amount[i]);
-				} 
-				catch (IOException e) 
-				{
-					System.out.println("IO error trying to read what item was purchased.");
-					System.exit(1);
-				}
 
-			// Ask the user if it is a food item
-				try
-				{
-					inputFlag = true;
-
-					while (inputFlag)
-					{
-						System.out.print("Is this a food, book, or medical item (Y or N): ");
-						input = br.readLine();
-						if (input.equals("Y") || input.equals("y") || input.equals("N") || input.equals("n"))
-						{
-							exempt[i] = input;
-							inputFlag = false;
-						}
-						else
-						{
-							System.out.print("Please enter 'Y' or 'N': ");
-						}
+					if (input.equals("Y") || input.equals("y") || input.equals("N") || input.equals("n")) {
+						imported[i] = input;
+						inputFlag = false;
 					}
-				} 
-				catch (IOException e) 
-				{
-					System.out.println("IO error trying to read what item was purchased.");
-					System.exit(1);
-				}
-
-			// Ask the user if it is an imported item
-				try
-				{
-					inputFlag = true;
-					while (inputFlag)
-					{
-						System.out.print("Is this an imported item (Y or N): ");
-						input = br.readLine();
-						if (input.equals("Y") || input.equals("y") || input.equals("N") || input.equals("n"))
-						{
-							imported[i] = input;
-							inputFlag = false;
-						}
-						else
-						{
-							System.out.print("Please enter 'Y' or 'N': ");
-						}
+					else {
+						System.out.print("Please enter 'Y' or 'N': ");
 					}
-				} 
-				catch (IOException e) 
-				{
-					System.out.println("IO error trying to read what item was purchased.");
-					System.exit(1);
 				}
 
-			// Find sales tax
-			// Round up to the nearest 0.05
+				// Find sales tax
+				// Round up to the nearest 0.05
 
-			if (exempt[i].equals("N") || exempt[i].equals("n"))
-			{
-				// Multiply by .1
-				// Add to total sales tax
-				unroundedSalesTax = (cost[i] * SALES_TAX);
-				salesTax[i] = salesTax[i] + ((Math.ceil(unroundedSalesTax*20))/20);
+				if (exempt[i].equals("N") || exempt[i].equals("n")) {
+					// Multiply by .1
+					// Add to total sales tax
+					unroundedSalesTax = (cost[i] * SALES_TAX);
+					salesTax[i] = salesTax[i] + ((Math.ceil(unroundedSalesTax * 20)) / 20);
 
-				/* Output to check for errors
-				System.out.println("Unrounded Sales Tax is: " + unroundedSalesTax);
-				System.out.println("Sales Tax is: " + salesTax[i]);*/
+					/*
+					 * Output to check for errors
+					 * System.out.println("Unrounded Sales Tax is: " + unroundedSalesTax);
+					 * System.out.println("Sales Tax is: " + salesTax[i]);
+					 */
+				}
+
+				if (imported[i].equals("Y") || imported[i].equals("y")) {
+					// Multiply by .05
+					// Add to total sales tax
+					// Change member to reflect sales tax
+					unroundedImportTax = (cost[i] * IMPORT_TAX);
+					salesTax[i] += ((Math.ceil(unroundedImportTax * 20)) / 20);
+
+					/*
+					 * Output to check for errors
+					 * System.out.println("Unrounded Import Tax is: " + unroundedImportTax);
+					 * System.out.println("Sales Tax is: " + salesTax[i]);
+					 */
+				}
+
+				totalSalesTax += salesTax[i];
+				taxedCost[i] = cost[i] + salesTax[i];
 			}
-
-			if (imported[i].equals("Y") || imported[i].equals("y"))
-			{
-				// Multiply by .05
-				// Add to total sales tax
-				// Change member to reflect sales tax
-				unroundedImportTax = (cost[i] * IMPORT_TAX);
-				salesTax[i] += ((Math.ceil(unroundedImportTax*20))/20);
-
-				/* Output to check for errors
-				System.out.println("Unrounded Import Tax is: " + unroundedImportTax);
-				System.out.println("Sales Tax is: " + salesTax[i]);*/
-			}
-
-			totalSalesTax += salesTax[i];
-			taxedCost[i] = cost[i] + salesTax[i];
+		}
+		catch (IOException e) {
+			System.out.println("IO error trying to read what item was purchased.");
+			System.exit(1);
 		}
 
-		
-		for (int i = 0; i < itemsPurchased; i++)
-		{
+		for (int i = 0; i < itemsPurchased; i++) {
 			totalCost += taxedCost[i];
 		}
 
 		System.out.print("\n" + "Receipt Details:" + "\n");
-		for (int i = 0; i < itemsPurchased; i++)
-		{
+		for (int i = 0; i < itemsPurchased; i++) {
 			System.out.print(amount[i] + " " + items[i] + ": " + df.format(taxedCost[i]) + "\n");
 		}
 		System.out.print("Sales Taxes: " + df.format(totalSalesTax) + "\n");
