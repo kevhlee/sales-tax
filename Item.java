8f54c7568646fb92cecca2b272417fb0b819f69e
Kevin Lee <kevin.lee@liferay.com>
2022-03-15 10:31:47 -0700

    Extract class (182): Convert item information into a class

---
+++ Item.java
@@ -0,0 +1,54 @@
+public class Item {
+
+    private final String name;
+    private final double price;
+    private final int quantity;
+    private boolean taxExempt;
+    private boolean imported;
+
+    public Item(String name, double price, int quantity) {
+        this(name, price, quantity, false, false);
+    }
+
+    public Item(String name, double price, int quantity, boolean taxExempt, boolean imported) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.taxExempt = taxExempt;
+        this.imported = imported;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public boolean isTaxExempt() {
+        return taxExempt;
+    }
+
+    public boolean isImported() {
+        return imported;
+    }
+
+    public void setImported(boolean imported) {
+        this.imported = imported;
+    }
+
+    public void setTaxExempt(boolean taxExempt) {
+        this.taxExempt = taxExempt;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
