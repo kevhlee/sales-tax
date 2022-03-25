7d2fbc2ec26b7cac67a0e851349ca8c6dc81ef09
Kevin Lee <kevin.lee@liferay.com>
2022-03-25 12:11:25 -0700

    Extract user input logic into methods

--- src/main/java/sales/tax/HelloWorld.java
+++ src/main/java/sales/tax/HelloWorld.java
@@ -33,19 +33,14 @@ class HelloWorld {
 	private static List<Item> createOrder() {
 		List<Item> items = new ArrayList<>();
 
-		String input = null;
 		int itemsPurchased = 0;
-		boolean inputFlag = true;
 
 		// Create a buffered reader to handle the input from the user
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		// Prompt user to enter how many items were purchased
-		System.out.print("Please enter how many items were purchased: ");
-
 		try {
-			input = br.readLine();
-			itemsPurchased = Integer.parseInt(input);
+			itemsPurchased = promptInteger(br, "Please enter how many items were purchased: ");
 		}
 		catch (IOException e) {
 			System.out.println("IO error trying to read how many items purchased.");
@@ -59,56 +54,11 @@ class HelloWorld {
 			for (int i = 0; i < itemsPurchased; i++) {
 				System.out.print("\n" + "sales.tax.Item #" + (i + 1) + "\n");
 
-				// Input the item purchased
-				System.out.print("What item did you purchase: ");
-
-				String name = br.readLine();
-
-				// Input how many were purchased
-				System.out.print("How many did you purchase: ");
-
-				input = br.readLine();
-				int quantity = Integer.parseInt(input);
-
-				// Input cost of item purchased
-				System.out.print("How much did it cost: ");
-
-				input = br.readLine();
-				double price = Double.parseDouble(input);
-
-				// Ask the user if it is a food item
-				inputFlag = true;
-				boolean exempt = false;
-
-				while (inputFlag) {
-					System.out.print("Is this a food, book, or medical item (Y or N): ");
-					input = br.readLine();
-
-					if (input.equals("Y") || input.equals("y") || input.equals("N") || input.equals("n")) {
-						exempt = input.equals("Y") || input.equals("y");
-						inputFlag = false;
-					}
-					else {
-						System.out.print("Please enter 'Y' or 'N': ");
-					}
-				}
-
-				// Ask the user if it is an imported item
-				inputFlag = true;
-				boolean imported = false;
-
-				while (inputFlag) {
-					System.out.print("Is this an imported item (Y or N): ");
-					input = br.readLine();
-
-					if (input.equals("Y") || input.equals("y") || input.equals("N") || input.equals("n")) {
-						imported = input.equals("Y") || input.equals("y");
-						inputFlag = false;
-					}
-					else {
-						System.out.print("Please enter 'Y' or 'N': ");
-					}
-				}
+				String name = promptInput(br, "What item did you purchase: ");
+				int quantity = promptInteger(br, "How many did you purchase: ");
+				double price = promptDouble(br, "How much did it cost: ");
+				boolean exempt = promptYesOrNo(br, "Is this a food, book, or medical item (Y or N): ");
+				boolean imported = promptYesOrNo(br, "Is this an imported item (Y or N): ");
 
 				Item item = new Item(name, price, quantity, exempt, imported);
 
@@ -121,5 +71,31 @@ class HelloWorld {
 		}
 
 		return items;
+	}
+
+	private static String promptInput(BufferedReader bufferedReader, String prompt) throws IOException {
+		System.out.print(prompt);
+		return bufferedReader.readLine();
+	}
+
+	private static int promptInteger(BufferedReader bufferedReader, String prompt) throws IOException {
+		return Integer.parseInt(promptInput(bufferedReader, prompt));
+	}
+
+	private static double promptDouble(BufferedReader bufferedReader, String prompt) throws IOException {
+		return Double.parseDouble(promptInput(bufferedReader, prompt));
+	}
+
+	private static boolean promptYesOrNo(BufferedReader bufferedReader, String prompt) throws IOException {
+		while (true) {
+			String input = promptInput(bufferedReader, prompt);
+
+			if (input.equals("Y") || input.equals("y") || input.equals("N") || input.equals("n")) {
+				return input.equals("Y") || input.equals("y");
+			}
+			else {
+				System.out.print("Please enter 'Y' or 'N': ");
+			}
+		}
 	}
 }
